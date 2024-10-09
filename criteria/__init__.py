f1eec40eb042b5a83f6920ec4becf4bb2dbc0dc9
miftahulumam <miftahul.umam14@gmail.com>
2024-10-09 15:21:18 +0900

    update criteria

--- criteria/__init__.py
+++ criteria/__init__.py
@@ -1,4 +1,4 @@
-# from .chamfer_distance_loss import chamfer_distance_loss
+from .chamfer_distance_loss import chamfer_distance_loss
 # from .quaternion_distance import quaternion_distance
 from .regression_loss import regression_loss
 from .rotation_loss import rotation_loss
--- criteria/chamfer_distance_loss.py
+++ criteria/chamfer_distance_loss.py
@@ -1,3 +1,33 @@
+# requirement: pip install chamferdist (https://github.com/krrish94/chamferdist)
+
+import torch
+import torch.nn as nn
+from chamferdist import ChamferDistance
+
+class chamfer_distance_loss(nn.Module):
+    def __init__(self, scale=1.0):
+        super(chamfer_distance_loss, self).__init__()
+        self.loss = ChamferDistance()
+        self.scale = scale
+
+    def forward(self, p1, p2):
+        total_loss = torch.tensor([0.0]).to(p1[0].device)
+        for i in range(len(p1)):
+
+            pcd1 = torch.unsqueeze(p1[i], dim=0)  
+            pcd2 = torch.unsqueeze(p2[i], dim=0)
+              
+            pcd1 = pcd1*self.scale
+            pcd2 = pcd2*self.scale
+
+            loss = self.loss(pcd1, pcd2, bidirectional=True)
+            total_loss += loss
+
+        return total_loss/len(p1)
+
+'''
+UNUSED VER. 
+
 # requirement: pip install git+'https://github.com/otaheri/chamfer_distance'
 
 import torch
@@ -24,3 +54,4 @@ class chamfer_distance_loss(nn.Module):
             total_loss += loss
 
         return total_loss/len(p1)
+'''
