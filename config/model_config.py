d62c859c59d2ba09c69ecd74d009f89d35a237bd
miftahulumam <miftahul.umam14@gmail.com>
2025-02-12 19:25:03 +0900

    update config

--- config/model_config.py
+++ config/model_config.py
@@ -36,3 +36,41 @@ config_UTransCalib_mobilenet_oct24 = {
 
     'init_weights': False
 }
+
+config_UTransCalib_mobilenet_nov3_a = {
+    'model_name': 'UTransCalib_mobilenet_nov3_a',
+
+    'rgb_activation': 'nn.SiLU(inplace=True)',
+    'depth_activation': 'nn.LeakyReLU(negative_slope=0.1, inplace=True)',
+    'fusion_activation': 'nn.SiLU(inplace=True)',
+    'regression_activation': 'nn.ReLU(inplace=True)',
+
+    'decoder_drop_rate': 0.1,
+    'fusion_reduction': 4,
+
+    'branch_attn_repeat': 0,
+    'fusion_attn_repeat': 2,
+
+    'head_drop_rate': 0.3,
+
+    'init_weights': False
+}
+
+config_UTransCalib_mobilenet_nov3_b = {
+    'model_name': 'UTransCalib_mobilenet_nov3_a',
+
+    'rgb_activation': 'nn.SiLU(inplace=True)',
+    'depth_activation': 'nn.LeakyReLU(negative_slope=0.1, inplace=True)',
+    'fusion_activation': 'nn.SiLU(inplace=True)',
+    'regression_activation': 'nn.ReLU(inplace=True)',
+
+    'decoder_drop_rate': 0.1,
+    'fusion_reduction': 4,
+
+    'branch_attn_repeat': 0,
+    'fusion_attn_repeat': 1,
+
+    'head_drop_rate': 0.3,
+
+    'init_weights': False
+}
